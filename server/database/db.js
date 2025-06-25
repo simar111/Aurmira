9a0889123083e94b17b24ba9f95ca4938124ed5f
Simar Narula <163547703+simar111@users.noreply.github.com>
2025-06-25 22:38:53 +0530

    done

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
   try {
-    const dbUri = process.env.MONGODB_URI || 'mongodb+srv://taruna:Taruna123@cluster0.3wyu8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    const dbUri = process.env.MONGODB_URI 
     
     await mongoose.connect(dbUri); 
     console.log('Connected to MongoDB');
